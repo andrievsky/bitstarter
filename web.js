1653412f17b089abc3585727eb9de00c01a31f27
andrievsky <toflasher@gmail.com>
2013-07-05 11:09:59 +0300

    read index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+//init
+const filePath = "index.html";
+var content = "Processing..."
+
+var reader = fs.readFile(filePath, { flag: 'r', encoding: "utf-8" }, function (err, data) 
+	{
+		if (err) throw err;
+		console.log("Read "+filePath+" complete");
+		content = data;
+	});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
